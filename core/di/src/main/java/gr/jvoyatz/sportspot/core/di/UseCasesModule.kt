3c5598a2045a89518e75001717c52d4a1a067249
jvoyatz <jvoyatz@gmail.com>
2023-03-12 00:27:53 +0200

    added (all)use cases in the dagger graph

---
+++ core/di/src/main/java/gr/jvoyatz/sportspot/core/di/UseCasesModule.kt
@@ -0,0 +1,34 @@
+package gr.jvoyatz.sportspot.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import gr.jvoyatz.sportpot.domain.usecases.usecases.GetSportEventById
+import gr.jvoyatz.sportpot.domain.usecases.usecases.GetSportEvents
+import gr.jvoyatz.sportpot.domain.usecases.usecases.RefreshSportEvents
+import gr.jvoyatz.sportpot.domain.usecases.usecases.SportEventsUseCases
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class UseCasesModule {
+
+    @ViewModelScoped
+    @Provides
+    fun provideGetSportEvents(useCases: SportEventsUseCases): GetSportEvents {
+        return useCases.getSportEvents
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun provideGetSportEventById(useCases: SportEventsUseCases): GetSportEventById {
+        return useCases.getSportEventById
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun provideRefreshSportEvents(useCases: SportEventsUseCases): RefreshSportEvents {
+        return useCases.refreshSportEvents
+    }
+}

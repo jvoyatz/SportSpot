c8f427bb92371fec5a80ad15cfc900cea5b930f5
jvoyatz <jvoyatz@gmail.com>
2023-03-12 15:51:33 +0200

    added sealed class for exceptions and its cases

--- domain/sport_events/model/src/main/java/gr/jvoyatz/sportspot/domain/model/Exceptions.kt
+++ domain/sport_events/model/src/main/java/gr/jvoyatz/sportspot/domain/model/Exceptions.kt
@@ -3,8 +3,14 @@ package gr.jvoyatz.sportspot.domain.model
 /**
  * A custom exception for handling error cases during fetching of sport events
  */
-class SportEventException: RuntimeException{
+sealed class SportEventException: RuntimeException{
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable) : super(message, cause)
     constructor(cause: Throwable) : super(cause)
+
+    class ErrorException(message: String = ""):SportEventException(message)
+    class NetworkException(message:String = ""):SportEventException(message)
+    class UnknownException(message:String = ""):SportEventException(message)
+    class HttpException(message:String = ""):SportEventException(message)
 }
+

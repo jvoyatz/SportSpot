9726ea28fa801d1de6a8ae6822f34a384f9b8cba
jvoyatz <jvoyatz@gmail.com>
2023-03-12 13:39:55 +0200

    updated mapper interface to be subclassed in order to avoid ambiguities warning from ide

--- core/common/src/main/java/gr/jvoyatz/sportspot/core/common/Mapper.kt
+++ core/common/src/main/java/gr/jvoyatz/sportspot/core/common/Mapper.kt
@@ -1,8 +1,18 @@
 package gr.jvoyatz.sportspot.core.common
 
 interface Mapper<From, To> {
-    fun From.mapToDomainModel(): To
-    fun To.mapFromDomainModel(): From
+//    fun mapToDomainModel(from: From): To
+//    fun mapFromDomainModel(to: To): From
+}
+
+interface DtoMapper<From, To> : Mapper<From, To>{
+    fun From.dtoToDomain(): To
+    fun To.domainToDto(): From
+}
+
+interface EntityMapper<From, To> : Mapper<From, To>{
+    fun From.entityToDomain(): To
+    fun To.domainToEntity(): From
 }
 
 inline fun <I, O> List<I>.mapList(mapSingle: (I) -> O): List<O> {

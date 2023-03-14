09fa093ca0d2d41c8a1cd4f066274ea4e2b83223
jvoyatz <jvoyatz@gmail.com>
2023-03-14 11:41:59 +0200

    added tests for SportEventRepository

--- data/sport_events/repo/src/test/java/gr/jvoyatz/sportspot/data/sports_events/repo/SportEventRepositoryImplFakeTest.kt
+++ data/sport_events/repo/src/test/java/gr/jvoyatz/sportspot/data/sports_events/repo/SportEventRepositoryImplFakeTest.kt
@@ -3,14 +3,20 @@ package gr.jvoyatz.sportspot.data.sports_events.repo
 import app.cash.turbine.test
 import com.google.common.truth.Truth
 import gr.jvoyatz.sportpot.domain.usecases.repository.SportEventsRepository
+import gr.jvoyatz.sportspot.core.database.entities.SportEventsEntity
 import gr.jvoyatz.sportspot.core.network.ApiResponse
 import gr.jvoyatz.sportspot.core.testing.utils.Utils
+import gr.jvoyatz.sportspot.data.sport_events.source.db.SportEventsDbClient
 import gr.jvoyatz.sportspot.data.sports_events.repo.fake.FakeApiClient
 import gr.jvoyatz.sportspot.data.sports_events.repo.fake.FakeDbClient
 import gr.jvoyatz.sportspot.data.sports_events.repo.mappers.BaseTest
 import gr.jvoyatz.sportspot.domain.model.SportEventException
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.*
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -165,6 +171,110 @@ class SportEventRepositoryImplFakeTest: BaseTest(){
             val event = awaitItem()
             Truth.assertThat(event).isNotNull()
         }
+    }
+
+    @Test
+    fun `mark event as a non favorite while marked as not favorite throws exception`() = runTest{
+
+        //given
+        //FOOT
+        val sportId = "FOOT"
+        val id = 29135390L
+
+        val fakeClient = object: SportEventsDbClient{
+            var wasCalled = false
+            override fun getSportEvents(): Flow<List<SportEventsEntity>> {
+                return flow {
+                    emit(entities!!)
+                }
+            }
+
+            override suspend fun deleteSportEvents() {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun insertSportEvents(events: List<SportEventsEntity>) {
+                wasCalled = true
+            }
+        }
+
+        //when
+        val repository = SportEventRepositoryImpl(apiClient, fakeClient)
+
+        //then
+        try{
+            repository.markSportEventAsFavorite(sportId, id, false)
+        }catch (e: Exception){
+            Truth.assertThat(e).isInstanceOf(SportEventException.ErrorException::class.java)
+        }
+        Truth.assertThat(fakeClient.wasCalled).isFalse()
+    }
+
+    @Test
+    fun `mark event as a favorite with success`() = runTest{
+        //given
+        val sportId = "FOOT"
+        val id = 29135390L
+
+        val fakeClient = object: SportEventsDbClient{
+            var wasCalled = false
+            override fun getSportEvents(): Flow<List<SportEventsEntity>> {
+                return flow {
+                    emit(entities!!)
+                }
+            }
+
+            override suspend fun deleteSportEvents() {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun insertSportEvents(events: List<SportEventsEntity>) {
+                wasCalled = true
+            }
+        }
+
+        //when
+        val repository = SportEventRepositoryImpl(apiClient, fakeClient)
+        repository.markSportEventAsFavorite(sportId, id, true)
+
+        //then
+
+        Truth.assertThat(fakeClient.wasCalled).isTrue()
+    }
+
+    @Test
+    fun `mark a not cached event as a favorite throws exception`() = runTest{
+        //given
+        val sportId = "FOOT"
+        val id = 29234135390L
+
+        val fakeClient = object: SportEventsDbClient{
+            var wasCalled = false
+            override fun getSportEvents(): Flow<List<SportEventsEntity>> {
+                return flow {
+                    emit(entities!!)
+                }
+            }
+
+            override suspend fun deleteSportEvents() {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun insertSportEvents(events: List<SportEventsEntity>) {
+                wasCalled = true
+            }
+        }
+
+        //when
+        val repository = SportEventRepositoryImpl(apiClient, fakeClient)
+        try{
+            repository.markSportEventAsFavorite(sportId, id, true)
+        }catch (e: Exception){
+            Truth.assertThat(e).isInstanceOf(SportEventException.ErrorException::class.java)
+        }
+
+        //then
+        Truth.assertThat(fakeClient.wasCalled).isFalse()
     }
 }
 
